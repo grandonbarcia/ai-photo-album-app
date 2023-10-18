e2645ec2e14fd019633a1b15f9f64c4f2304d68e
Brandon Garcia <grandonleebarcia@gmail.com>
2023-10-18 14:04:44 -0700

    show images on gallery page

--- src/app/gallery/page.tsx
+++ src/app/gallery/page.tsx
@@ -1,5 +1,6 @@
 import UploadButton from './upload-button';
 import cloudinary from 'cloudinary';
+import { CloudinaryImage } from './cloudinary-image';
 
 type SearchResult = {
   public_id: string;
@@ -8,15 +9,30 @@ type SearchResult = {
 export default async function GalleryPage() {
   const results = (await cloudinary.v2.search
     .expression('resource_type:image')
-    .sort_by('public_id', 'desc')
-    .max_results(30)
-    .execute()) as SearchResult[];
+    .sort_by('created_at', 'desc')
+    .max_results(10)
+    .execute()) as { resources: SearchResult[] };
 
   return (
     <section>
-      <div className="flex justify-between">
-        <h1 className="text-4xl font-bold"> Gallery </h1>
-        <UploadButton />
+      <div className="flex flex-col gap-8">
+        <div className="flex justify-between">
+          <h1 className="text-4xl font-bold"> Gallery </h1>
+          <UploadButton />
+        </div>
+        <div className="grid grid-cols-4 gap-4">
+          {results.resources.map((result) => {
+            return (
+              <CloudinaryImage
+                key={result.public_id}
+                src={result.public_id}
+                alt="an image of something"
+                width="400"
+                height="300"
+              />
+            );
+          })}
+        </div>
       </div>
     </section>
   );
